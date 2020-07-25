c90e8704a27ccb6e5e58ef42c1aaa9dd19bc3a74
Ethan Uphoff <ethan.uphoff@gmail.com>
2020-07-25 13:33:58 -0500

    add Firebase and a simple Firebase DB call example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firestore from './firebase';
+
+const db = firestore.firestore()
+db.collection("Users")
+.get()
+.then(querySnapshot => {
+  const data = querySnapshot.docs.map(doc => doc.data());
+  console.log(data); // array of cities objects
+});
+//db.collection("Users").add({Username:"Test", Twitch:"Test"})
+
 
 function App() {
   return (
