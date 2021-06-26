a0945e90f15ac22e9bedf170f177b4857ea27e25
unknown <adanty@excilys.com>
2021-06-26 21:25:09 +0200

    Setting up nextJS

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import firestore from '../firebase';
+
+const db = firestore.firestore()
+db.collection("Users")
+    .get()
+    .then(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => doc.data());
+        console.log(data); // array of cities objects
+    });
+//db.collection("Users").add({Username:"Test", Twitch:"Test"})
+
+function HomePage() {
+    return (
+        <div>Welcome to Next.js!</div>
+    )
+}
+
+export default HomePage
