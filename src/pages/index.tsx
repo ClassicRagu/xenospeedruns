03d8f7c7ff95797a633ae6f89900e8cf04f07abc
unknown <adanty@excilys.com>
2021-06-26 22:16:35 +0200

    Creating all three pages and links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import firestore from '../firebase';
+import React from "react";
+import Link from "next/link";
+import firestore from "../firebase";
 
-const db = firestore.firestore()
+const db = firestore.firestore();
 db.collection("Users")
     .get()
-    .then(querySnapshot => {
-        const data = querySnapshot.docs.map(doc => doc.data());
+    .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => doc.data());
         console.log(data); // array of cities objects
     });
 //db.collection("Users").add({Username:"Test", Twitch:"Test"})
 
 function HomePage() {
     return (
-        <div>Welcome to Next.js!</div>
-    )
+        <>
+            <div>Welcome to Xenospeedruns.com! Powered by Next.js!</div>
+            <Link href="/Games">Games</Link>
+            <br />
+            <Link href="/Runners">Runners</Link>
+        </>
+    );
 }
 
 export default HomePage;
