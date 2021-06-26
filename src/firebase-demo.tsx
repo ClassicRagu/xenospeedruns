c457ea3eb95053729953217688392ed8c33559db
unknown <adanty@excilys.com>
2021-06-26 22:11:48 +0200

    Prettier!

--- src/firebase-demo.tsx
+++ src/firebase-demo.tsx
@@ -1,10 +1,10 @@
 import firestore from "./firebase";
 
-const db = firestore.firestore()
+const db = firestore.firestore();
 db.collection("Users")
     .get()
-    .then(querySnapshot => {
-        const data = querySnapshot.docs.map(doc => doc.data());
+    .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => doc.data());
         console.log(data); // array of cities objects
     });
 //db.collection("Users").add({Username:"Test", Twitch:"Test"})
